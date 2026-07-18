3f1dee23a71592b97e24b610dd986c303ca606ac
zenith go <35967220+jooa1018@users.noreply.github.com>
2026-07-18 12:12:49 +0900

    wip(phase55): add GPT-first textbook parser

---
+++ backend/engine/textbook_parser/validators/__init__.py
@@ -0,0 +1,4 @@
+from engine.textbook_parser.validators.safety import validate_payload_authority
+from engine.textbook_parser.validators.semantic import validate_semantics
+
+__all__ = ["validate_payload_authority", "validate_semantics"]
